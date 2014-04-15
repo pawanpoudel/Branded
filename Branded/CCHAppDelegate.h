c996cd27e997d8875adc00e6c1fe5749beb4085d
Pawan Poudel <pawan.poudel@gmail.com>
2014-04-14 21:40:39 -0400

    Create a new Xcode project

---
+++ Branded/CCHAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CCHAppDelegate.h
+//  Branded
+//
+//  Created by PAWAN POUDEL on 4/14/14.
+//  Copyright (c) 2014 Pawan Poudel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Branded/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Branded
+//
+//  Created by PAWAN POUDEL on 4/14/14.
+//  Copyright (c) 2014 Pawan Poudel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCHAppDelegate class]));
+    }
+}
