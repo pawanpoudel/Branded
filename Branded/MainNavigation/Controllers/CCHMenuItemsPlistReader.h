a4f1631cd20912b11a5b9ef12f120833c9667a95
Pawan Poudel <pawan.poudel@gmail.com>
2014-04-27 14:37:52 -0500

    Document CCHMenuItemsPlistReader's public interface

--- Branded/MainNavigation/Controllers/CCHMenuItemsPlistReader.h
+++ Branded/MainNavigation/Controllers/CCHMenuItemsPlistReader.h
@@ -8,8 +8,17 @@
 
 #import "CCHMenuItemsReader.h"
 
+/*!
+    @description Read menu items metadata from the specified plist.
+    @discussion This class conforms to CCHMenuItemsReader protocol.
+ */
+
 @interface CCHMenuItemsPlistReader : NSObject <CCHMenuItemsReader>
 
+/*!
+    @description A NSString object that contains the name of the
+                 plist file to read menu items metadata from.
+ */
 @property (nonatomic) NSString *plistToReadFrom;
 
 @end
